03af6e2213a6d0842d5f9b7c897ef43faec11029
Normaidian <narvall666@gmail.com>
2018-07-28 07:37:07 +0200

    2.9
    Fixed printed tabel with modules

--- module.h
+++ module.h
@@ -37,18 +37,29 @@ class Module{
 
             std::string floor = "_____", space = "     ";
 
-            for (int unsigned i = 0; i < length; i++){
-                floor = floor + "_";
-                space = space + " ";
+            if (length > 19){
+                for (int unsigned i = 0; i < length; i++){
+                    floor = floor + "_";
+                    space = space + " ";
+                }
+            }else{
+                for (int unsigned i = 0; i < 19; i++){
+                    floor = floor + "_";
+                    space = space + " ";
+                }
             }
 
             std::cout << " _______" << floor << "_____________________________________"  << std::endl;
-            std::cout << "| ID   |      Module name" << space.substr(0,length - 12) << "|    Base address     | Memory Class |"  << std::endl;
+            std::cout << "| ID   |      Module name  " << space.substr(0,space.size() - 19) << "|    Base address     | Memory Class |"  << std::endl;
             std::cout << "|______|" << floor << "|_____________________|______________|"  << std::endl;
 
             for(std::list<Module>::iterator i = moduleList.begin(); i!=moduleList.end();++i){
                 Module m = *i;
-                std::cout << "| " << m.id << space.substr(0,5 - std::to_string(m.id).length()) << "|   " << m.name << space.substr(0, length - m.name.length()) << "  |      " << m.baseaddress << "     |  " << m.memoryclass << space.substr(0,12 - m.memoryclass.length()) <<  "|" << std::endl;
+
+                std::cout << "| " << m.id << space.substr(0,5 - std::to_string(m.id).length()) <<
+                "|   " << m.name << space.substr(0, space.size() - m.name.length() - 3) <<
+                "|      " << m.baseaddress << space.substr(0, (space.size() - (space.size() - 21) - m.baseaddress.length() - 6)) <<
+                "|  " << m.memoryclass << space.substr(0,12 - m.memoryclass.length()) <<  "|" << std::endl;
                 std::cout << "|______|" << floor << "|_____________________|______________|"  << std::endl;
             }
         }
