82c41d413b1b525d72f925e16709b5a32f281c3e
Normaidian <narvall666@gmail.com>
2018-08-08 09:54:57 +0200

    v3.1
    added mCreator()

--- main.cpp
+++ main.cpp
@@ -15,6 +15,8 @@ string line, fileAddress, baseAddress, tempForLine, tempGroupLine, coreAddress =
 
 void registerTabel();
 void moduleTabel();
+void mCreator();
+list <Module> moduleList(string fileAddress);
 
 HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
@@ -33,6 +35,8 @@ int main(){
             cout << "|___________________________________________________|" << endl;
             cout << "|    2 - Get table with modules   (for *.p file)    |" << endl;
             cout << "|___________________________________________________|" << endl;
+            cout << "|    3 - *.m file creator                           |" << endl;
+            cout << "|___________________________________________________|" << endl;
             cout << "|    0 - Exit                                       |" << endl;
             cout << "|___________________________________________________|" << endl;
             cout << "Select operation: ";
@@ -48,6 +52,9 @@ int main(){
                     break;
                     case 2:
                         moduleTabel();
+                    break;
+                    case 3:
+                        mCreator();
                     break;
                     case 0:
                         exit(0);
@@ -67,6 +74,68 @@ int main(){
         }
     }while(true);
 return 0;
+}
+
+list <Module> moduleList(string fileAddress, string separator){
+    string name, tmpName, base = "0x00", memoryClass, address;
+    list <string> parameters;
+    list <Module> modules;
+    list <string> trees;
+    bool isModule = false;
+    fstream file;
+
+    file.open(fileAddress.c_str(), ios::in);
+
+    while(getline(file,line)){
+        if((line.find("tree.end") != string::npos)&&(isModule == true)){
+
+            string fullName;
+
+            for(list<string>::iterator i = trees.begin(); i != trees.end(); ++i){
+                fullName = fullName + *i + separator;
+            }
+            fullName.erase(fullName.length()-separator.size(),separator.size());
+
+            Module module(fullName, base, address, memoryClass, parameters);
+
+            modules.push_back(module);
+            trees.pop_back();
+            parameters.clear();
+            isModule = false;
+        }else if((line.find("tree.end") != string::npos)){
+            trees.pop_back();
+        }else if(line.find("tree") != string::npos){
+            line = line.substr(line.find('"') + 1,line.size());
+            trees.push_back(line.substr(0,line.find('"')));
+        }else if(line.find("base ") != string::npos){
+            base = line.substr(line.find(":") + 1,line.length());
+            base.erase(std::remove(base.begin(), base.end(), ' '), base.end());
+            memoryClass = line.substr(line.find("base ") + 5,line.find(":") - line.find("base ")-5);
+        }else if(line.find("%include ") != string::npos){
+
+            if(line.find("${TOPDIR}") != string::npos){
+                address = line.substr(line.find("%include ") + 9, line.find(".ph") - line.find("%include ") - 6);
+                address.replace(address.find("${TOPDIR}"),9,topdir);
+            }else{
+                address = fileAddress.substr(0,fileAddress.find_last_of("/\\") + 1) + line.substr(line.find("%include ") + 9, line.find(".ph") - line.find("%include ") - 6);
+            }
+
+            if(line.find(".ph ") != string::npos){
+                line = line.substr(line.find(".ph") + 4, line.size());
+                line = line + " ";
+
+                while(!line.empty()){
+                    string param = line.substr(0,line.find(" "));
+                    parameters.push_back(param);
+                    line = line.erase(0,line.find(param)+param.size()+1);
+                }
+            }
+            isModule = true;
+        }
+    }
+
+    file.close();
+    return modules;
 }
 
 void registerTabel(){
@@ -135,7 +204,7 @@ void registerTabel(){
 
 void moduleTabel(){
     fstream file;
-
+    list <Module> modules;
     Module::actualId = 0;
 
     SetConsoleTextAttribute( hOut, 10 );
@@ -143,73 +212,17 @@ void moduleTabel(){
     SetConsoleTextAttribute( hOut, 7);
     cin >> fileAddress;
 
-
     if((!file.good())||(fileAddress.substr(fileAddress.size() - 2, 2) != ".p")){
         throw(std::logic_error("---Wrong file address!---"));
     }
 
-    file.open(fileAddress.c_str(), ios::in);
-
     topdir = fileAddress.substr(0,10);
 
-    string name, tmpName, base = "0x00", memoryClass, address;
-    list <string> parameters;
-    list <Module> modules;
-    list <string> trees;
-    bool isModule = false;
 
     system("cls");
 
     do{
-        while(getline(file,line)){
-            if((line.find("tree.end") != string::npos)&&(isModule == true)){
-
-                string fullName;
-
-                for(list<string>::iterator i = trees.begin(); i != trees.end(); ++i){
-                    fullName = fullName + *i + " - ";
-                }
-                fullName.erase(fullName.length()-3,fullName.length());
-
-                Module module(fullName, base, address, memoryClass, parameters);
-
-                modules.push_back(module);
-                trees.pop_back();
-                parameters.clear();
-                isModule = false;
-            }else if((line.find("tree.end") != string::npos)){
-                trees.pop_back();
-            }else if(line.find("tree") != string::npos){
-                line = line.substr(line.find('"') + 1,line.size());
-                trees.push_back(line.substr(0,line.find('"')));
-            }else if(line.find("base ") != string::npos){
-                base = line.substr(line.find(":") + 1,line.length());
-                base.erase(std::remove(base.begin(), base.end(), ' '), base.end());
-                memoryClass = line.substr(line.find("base ") + 5,line.find(":") - line.find("base ")-5);
-            }else if(line.find("%include ") != string::npos){
-
-                if(line.find("${TOPDIR}") != string::npos){
-                    address = line.substr(line.find("%include ") + 9, line.find(".ph") - line.find("%include ") - 6);
-                    address.replace(address.find("${TOPDIR}"),9,topdir);
-                }else{
-                    address = fileAddress.substr(0,fileAddress.find_last_of("/\\") + 1) + line.substr(line.find("%include ") + 9, line.find(".ph") - line.find("%include ") - 6);
-                }
-
-                if(line.find(".ph ") != string::npos){
-                    line = line.substr(line.find(".ph") + 4, line.size());
-                    line = line + " ";
-
-                    while(!line.empty()){
-                        string param = line.substr(0,line.find(" "));
-                        parameters.push_back(param);
-                        line = line.erase(0,line.find(param)+param.size()+1);
-                    }
-                }
-                isModule = true;
-            }
-        }
-
-        file.close();
+        modules = moduleList(fileAddress, " - ");
         system("cls");
 
         Module::print(modules);
@@ -253,4 +266,96 @@ void moduleTabel(){
             }
         }
     }while(true);
+}
+
+void mCreator(){
+    fstream file;
+    string mFileAddress, space = "        ", prevPopup, actPopup, mcuName, mcuProps, mcuAuthor, mcuManufacturer, mcuCore;
+    list <Module> modules;
+    unsigned int level = 0;
+    bool first = false;
+
+    SetConsoleTextAttribute( hOut, 10 );
+    cout << endl << "*.p file address: ";
+    SetConsoleTextAttribute( hOut, 7);
+    cin >> fileAddress;
+
+    if((!file.good())||(fileAddress.substr(fileAddress.size() - 2, 2) != ".p")){
+        throw(std::logic_error("---Wrong file address!---"));
+    }
+
+    file.open(fileAddress, ios::in);
+
+    while(getline(file,line)){
+        if (line.find("@Title:") != string::npos){
+            mcuName = line.substr(line.find(": ") + 2,line.find(" On-Chip Peripherals") - line.find(": ") - 2);
+        }else if(line.find("@Props:") != string::npos){
+            mcuProps = line.substr(line.find(": ") + 2,line.size());
+        }else if(line.find("@Author:") != string::npos){
+           // SetConsoleTextAttribute( hOut, 10 );
+           // cout << "Author: ";
+           // SetConsoleTextAttribute( hOut, 7);
+           // cin >> mcuAuthor;
+           mcuAuthor = "adas";
+        }else if(line.find("@Manufacturer:") != string::npos){
+            mcuManufacturer = line.substr(line.find(": ") + 2,line.size());
+        }else if(line.find("@Core:") != string::npos){
+            mcuCore = line.substr(line.find(": ") + 2,line.size());
+        }
+    }
+
+    file.close();
+
+    mFileAddress = fileAddress;
+    mFileAddress = mFileAddress.replace(mFileAddress.find(".p"),2,".m");
+    mFileAddress.replace(mFileAddress.rfind("per"),3,"men");
+
+    SYSTEMTIME st;
+    GetLocalTime(&st);
+
+    file.open(mFileAddress, ios::out);
+
+    file << "; --------------------------------------------------------------------------------" << endl;
+    file << "; @Title: " << mcuName << " Specific Menu" << endl;
+    file << "; @Props: " << mcuProps << endl;
+    file << "; @Author: " << mcuAuthor << endl;
+    file << "; @Changelog: " << st.wYear << "-" << st.wMonth << "-" << st.wDay << " " << mcuAuthor << endl;
+    file << "; @Manufacturer: " << mcuManufacturer << endl;
+    file << "; @Core: " << mcuCore << endl;
+    file << "; @Copyright: (C) 1989-" << st.wYear << " Lauterbach GmbH, licensed for use with TRACE32(R) only" << endl;
+    file << "; --------------------------------------------------------------------------------" << endl;
+    file << "; $Id: menesirisc.m 29969 2018-07-31 13:12:25Z SIK $" << endl;
+    file << endl;
+    file << "add" << endl;
+    file << endl;
+    file << "menu" << endl;
+    file << "(" << endl;
+    file << "    popup \"" << mcuName << "\"" << endl;
+    file << "    (" << endl;
+
+    modules = moduleList(fileAddress, ",");
+
+    for(std::list<Module>::iterator i = modules.begin(); i!=modules.end();++i){
+        Module m = *i;
+        string name = m.name;
+
+
+        if((name.find(",") != string::npos)){
+            actPopup = name.substr(0,name.find_last_of(","));
+
+            if(prevPopup != actPopup){
+                prevPopup = actPopup;
+            }
+            name.erase(0,name.find_last_of(",") + 1);
+        }
+
+        file << space << "menuitem \"" << name << "\"                \"per , \"\"" << m.name << "\"\"\"" << endl;
+    }
+
+    file << "    )" << endl;
+    file << ")" << endl;
+
+    file.close();
+
+    exit(0);
 }
